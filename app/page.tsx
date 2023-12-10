541417d7ec776fcdde6964bc7bd068de3e3d2aa7
typle <aungpyaephyo1412@gmail.com>
2023-12-10 20:36:03 +0630

    init : v1

---
+++ app/page.tsx
@@ -0,0 +1,220 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import IMG1 from "../public/Images/1.jpg"
+import IMG2 from "../public/Images/2.jpg"
+import IMG3 from "../public/Images/3.jpg"
+import IMG4 from "../public/Images/4.jpg"
+import IMG5 from "../public/Images/5.jpg"
+import IMG6 from "../public/Images/6.jpg"
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+function BlogLink({ slug, name }) {
+  return (
+    <div className="group">
+      <a
+        href={`/blog/${slug}`}
+        className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
+      >
+        <div className="flex flex-col">
+          <p className="font-medium text-neutral-900 dark:text-neutral-100">
+            {name}
+          </p>
+        </div>
+        <div className="text-neutral-700 dark:text-neutral-300 transform transition-transform duration-300 group-hover:-rotate-12">
+          <ArrowIcon />
+        </div>
+      </a>
+    </div>
+  );
+}
+
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        hey, I'm Thihan 👋
+      </h1>
+      <p className="prose prose-neutral dark:prose-invert">
+        {`
+          Hi! My name is Duy Nguyen, a young creative developer from Vietnam. I am passionate about creating visually stunning and innovative websites that not only look great but also provide exceptional user experiences.
+        `}
+      </p>
+      <div className="columns-2 sm:columns-3 gap-4 my-8">
+        <div className="relative h-40 mb-4">
+          <Image
+            alt="Me speaking on stage at React Summit about the future of Next.js"
+            src={IMG1}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-80 mb-4 sm:mb-0">
+          <Image
+            alt="Me, Lydia, and Delba filming the Next.js Conf keynote"
+            src={IMG2}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-[-16px] sm:object-center"
+          />
+        </div>
+        <div className="relative h-40 sm:h-80 sm:mb-4">
+          <Image
+            alt="Me standing on stage at Reactathon delivering the keynote"
+            src={IMG3}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-top sm:object-center"
+          />
+        </div>
+        <div className="relative h-40 mb-4 sm:mb-0">
+          <Image
+            alt="Me standing on stage at SmashingConf giving a talk about my optimism for the web"
+            src={IMG4}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-40 mb-4">
+          <Image
+            alt="Me and Guillermo Rauch on stage for Vercel Ship, answering questions from the Next.js community"
+            src={IMG5}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-80">
+          <Image
+            alt="My badge on top of a pile of badges from a Vercel meetup we held"
+            src={IMG6}
+            fill
+            sizes="(min-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          I create educational content for developers, teaching them about web
+          development, JavaScript and TypeScript, React and Next.js, and more.
+          This comes in all forms: blog posts, videos, tweets, conference talks,
+          and workshops. You can watch some of my favorites below.
+        </p>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          Over the past decade, I've written content on my blog and newsletter.
+          I try to keep things simple. You'll find writing about technologies
+          I'm interested in at the time, or how I'm learning and growing in my
+          career, sharing knowledge along the way.
+        </p>
+      </div>
+      {/*<div className="my-8 flex flex-col space-y-4 w-full">*/}
+      {/*  <BlogLink*/}
+      {/*    name="What Makes A Great Developer Experience?"*/}
+      {/*    slug="developer-experience-examples"*/}
+      {/*  />*/}
+      {/*  <BlogLink name="What is Developer Relations?" slug="devrel-at-vercel" />*/}
+      {/*  <BlogLink name="The Story of Heroku" slug="heroku" />*/}
+      {/*</div>*/}
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          I invest small angel checks into early stage startups building tools
+          for developers.
+        </p>
+      </div>
+      <div className="my-8 flex flex-row space-x-2 w-full h-14 overflow-x-auto">
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
+          <a href="https://linear.app">
+            <svg width="78" height="20" role="img" aria-label="Linear logo">
+              <use href="/sprite.svg#linear" />
+            </svg>
+          </a>
+        </div>
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
+          <a href="https://supabase.com">
+            <svg width="100" height="19" role="img" aria-label="Supabase logo">
+              <use href="/sprite.svg#supabase" />
+            </svg>
+          </a>
+        </div>
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
+          <a href="https://www.makeswift.com/blog/makeswift-is-joining-bigcommerce">
+            <svg width="96" height="19" role="img" aria-label="Makeswift logo">
+              <use href="/sprite.svg#makeswift" />
+            </svg>
+          </a>
+        </div>
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
+          <a href="https://resend.com">
+            <svg width="70" height="17" role="img" aria-label="Resend logo">
+              <use href="/sprite.svg#resend" />
+            </svg>
+          </a>
+        </div>
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
+          <a href="https://bun.sh">
+            <svg width="35" height="27" role="img" aria-label="Bun logo">
+              <use href="/sprite.svg#bun" />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          I've worked with and advised companies on developer marketing,{' '}
+          <Link href="/blog/devrel-at-vercel">developer relations</Link>,
+          building open-source communities, product-led growth, and more.
+        </p>
+      </div>
+      <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-600 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/leeerob"
+          >
+            <ArrowIcon />
+            <p className="h-7 ml-2">follow me</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://leerob.substack.com"
+          >
+            <ArrowIcon />
+            <p className="h-7 ml-2">send email</p>
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
